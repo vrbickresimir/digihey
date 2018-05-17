99bf7fc53ae2285167ef1630162d49be304d7d61
kvrbic <vrbickresimir@gmail.com>
2018-05-17 23:42:47 +0200

    Added statusChange feature and added navigation to the transformer comp

--- src/app/transformer/transformer.component.ts
+++ src/app/transformer/transformer.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 import { Transformer } from '../models/transformer.class';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-transformer',
@@ -8,10 +12,28 @@ import { Transformer } from '../models/transformer.class';
 })
 export class TransformerComponent implements OnInit {
   @Input() transformer: Transformer;
+  statusForm: FormGroup;
 
-  constructor() { }
+  constructor(private appService: AppService, private router: Router) { }
 
   ngOnInit() {
+    this.setStatus()
+  }
+
+  setStatus() {
+    let status = this.transformer.status;
+    this.statusForm = new FormGroup({
+      'status': new FormControl(status, Validators.required)
+    });
+  }
+
+  onSelect(transformer: Transformer, index: number): void {
+    this.appService.selectedItem.next(transformer);
+    this.router.navigate(['edit', index]);
+  }
+
+  saveStatus() {
+    console.log('nada');
   }
 
 }
