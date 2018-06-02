4e87e83c0dafef3e322a42465c49c4bd9e802d9c
kvrbic <vrbickresimir@gmail.com>
2018-06-02 19:45:15 +0200

    Formated preview component to be more efficient

--- src/app/preview/preview.component.ts
+++ src/app/preview/preview.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
-
-import { Transformer } from '../models/transformer.class';
-import { AppService } from '../app.service';
 import { Subscription } from 'rxjs';
 import { Observable } from 'rxjs/Observable';
 import { map, startWith } from 'rxjs/operators';
 
+import { Transformer } from '../models/transformer.class';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-preview',
@@ -18,29 +17,17 @@ export class PreviewComponent implements OnInit, OnDestroy {
   transformers: Transformer[];
   filteredTransformers: Observable<Transformer[]>;
   subscription: Subscription = new Subscription();
-  options: string[] = [];
   filterControl: FormControl = new FormControl();
-  filteredOptions: Observable<string[]>;
-
 
   constructor(public appService: AppService) { }
 
   ngOnInit() {
     this.subscription.add(this.appService.getTransformers().subscribe(data => {
       this.transformers = data;
-    },null, () => {
-      this.transformers.forEach( transformer => {
-        console.log(transformer);
-        this.options.push(transformer.name);
-      })
-      this.filteredOptions = this.filterControl.valueChanges
-      .pipe(
-        startWith(''),
-        map(val => this.filter(val))
-      );
+    }, null, () => {
       this.filteredTransformers = this.filterControl.valueChanges.pipe(
         startWith(''),
-        map(value => this.filter2(value))
+        map(value => this.filter(value))
       )
     }));
   }
@@ -49,12 +36,8 @@ export class PreviewComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  filter(val: string): string[] {
-    return this.options.filter(option =>
-      option.toLowerCase().includes(val.toLowerCase()));
-  }
-  filter2(name: string): Transformer[] {
-    return this.transformers.filter( transformer => 
+  filter(name: string): Transformer[] {
+    return this.transformers.filter(transformer =>
       transformer.name.toLocaleLowerCase().includes(name.toLocaleLowerCase()));
   }
 
